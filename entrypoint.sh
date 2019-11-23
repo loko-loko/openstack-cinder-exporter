bff8af6bf9e0aa8db4a892317e7938e8b24ec212
Laurent-Alou Koné <laurent.alou.k@gmail.com>
2019-11-23 17:24:02 -0500

    fix: change condition type + debug option on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
-ARG_DEBUG=""
+#!/bin/sh
+
+set -e
+
 CLOUDS_CONFIG="/config/clouds.yaml"
 
-if [[ ! -e $CLOUDS_CONFIG ]]; then
+if [ ! -e "$CLOUDS_CONFIG" ]; then
     echo "<!> Clouds config file missing: $CLOUDS_CONFIG"
     exit 1
 fi
 
-[[ $DEBUG == 1 ]] && ARG_DEBUG="--debug"
+set -- "--port 8000" "--cloud-config $CLOUDS_CONFIG"
 
-set - "--cloud-config $CLOUDS_CONFIG" "--port 8000" "$ARG_DEBUG"
+[ "${DEBUG:-0}" -eq 1 ] && set -- "$@" "--debug"
 
 COMMAND="python -m cinder_exporter $@"
 
 echo "exec: $COMMAND"
-$COMMAND
+exec $COMMAND
